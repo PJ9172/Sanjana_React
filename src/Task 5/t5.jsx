bfa004159804c53aa7a3c99290a826ae02a333fd
Prajwal <rohitjagatap5000@gmail.com>
2024-03-22 00:33:17 +0530

    first commit

--- src/Task 5/t5.jsx
+++ src/Task 5/t5.jsx
@@ -1,25 +1,21 @@
 import { useState } from "react"
 
-export default function Form(){
-    var [n,setName]=useState("Student")
+export default function Form() {
+    var [n, setName] = useState("Student")
 
     return (
-    <>
-        <h4>Name : {n}</h4>
+        <>
+            <h4>Name : {n}</h4>
 
-        <input type="text" id="name" value ={n} onChange={()=>{
-            var name=document.getElementById('name');
-            console.log(name.value);
-            setName(name.value)
+            <input type="text" id="name" value={n} onChange={() => {
+                var name = document.getElementById('name');
+                console.log(name.value);
+                setName(name.value)
+            }} /> <br /><br />
+            <button onClick={() => {
+                setName("Student")
+            }}>Reset</button>
 
-        }}/> <br /><br />
-        
-
-        <button onClick={()=>{
-            setName("Student")
-        }}>Reset</button>
-        
-    </>
-
+        </>
     )
 }
